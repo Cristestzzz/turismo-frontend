da999b0ccdd546c99151aa84c51bbfe05027e7f9
i1lmatic <patrickrobinhopaniquepuma@gmail.com>
2025-08-16 15:17:02 -0500

    favoritos incompelto
    fvoritos inompleto

--- src/api/favorito.ts
+++ src/api/favorito.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/favoritos';
+const API_URL = 'http://localhost:8000/favoritos/';
 
 export async function addFavorito(paquete_id: number, token: string) {
 	return axios.post(
@@ -12,15 +12,15 @@ export async function addFavorito(paquete_id: number, token: string) {
 
 export async function removeFavorito(paquete_id: number, token: string) {
 	return axios.delete(
-		`${API_URL}/${paquete_id}`,
+		`${API_URL}${paquete_id}`,
 		{ headers: { Authorization: `Bearer ${token}` } }
 	);
 }
 
 export async function checkFavorito(paquete_id: number, token: string) {
-  const res = await axios.get(
-    `/favoritos/check/${paquete_id}`,
-    { headers: { Authorization: `Bearer ${token}` } }
-  );
-  return res.data;
+	const res = await axios.get(
+		`${API_URL}check/${paquete_id}`,
+		{ headers: { Authorization: `Bearer ${token}` } }
+	);
+	return res.data;
 }
